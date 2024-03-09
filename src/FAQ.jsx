d5e857eb7fae1c0678064596b3bf12cad66dbae1
suraj7974 <surajpatelsuraj55@gmail.com>
2024-03-09 20:45:34 +0530

    addded FAQs page

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,55 +1,65 @@
 import React from "react";
 import "./FAQ.css";
+import { useState } from "react";
 
 function FAQ() {
+  const [selected, setSelected] = useState(null);
+  const toggle = (i) => {
+    if (selected == i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
   return (
-    <><div className="topic" align="center">FAQ'S</div>
-      <div className="FAQ">
-        <div className="main_container">
-          <div className="faq_top">
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
+    <><div className="title_faq" align="center">FAQs</div>
+      <div className="wrapper">
+        <div className="faq">
+          {data.map((item, i) => (
+            <div className="item">
+              <div className="title" onClick={() => toggle(i)}>
+                <h2>{item.question}</h2>
+                <span className="sign">{selected === i ? "-" : "+"}</span>
+              </div>
+              <div className={selected === i ? "content show" : "content"}>
+                {item.answer}
+              </div>
             </div>
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
-            </div>
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
-            </div>
-          </div>
-          <div className="faq_bottom">
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
-            </div>
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
-            </div>
-            <div className="question_ans">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-              fuga illo unde voluptate nobis deleniti laboriosam saepe tenetur
-              sequi neque.
-              <button className="ans-btn">ANSWER</button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
   );
 }
-
+const data = [
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+  {
+    question: "what is HC?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam ut maiores ad rem voluptatum? Molestiae debitis eius ut molestias voluptatum ducimus quam maxime repellendus fugiat? Quo aliquam rem nam?",
+  },
+];
 export default FAQ;
