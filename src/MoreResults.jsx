7421bced4f513c5906774626cf4d078900ecb57b
suraj7974 <surajpatelsuraj55@gmail.com>
2024-03-09 18:06:04 +0530

    addded a elemnt for more results for better understanding

---
+++ src/MoreResults.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./MoreResults.css";
+
+function MoreResults() {
+  return (
+    <div>
+        
+      <table className="seemore-table" cellSpacing="0" align="center" cellpadding="10px">
+        <tr className="rows">
+          <th className="columns">Column 1</th>
+          <th className="columns">Column 2</th>
+          <th className="columns">Column 3</th>
+          <th className="columns">Column 4</th>
+          <th className="columns">Column 5</th>
+          <th className="columns">Column 6</th>
+        </tr>
+        <tr className="columns"lassName="rows">
+          <td className="columns">Data 1</td>
+          <td className="columns">Data 2</td>
+          <td className="columns">Data 3</td>
+          <td className="columns">Data 4</td>
+          <td className="columns">Data 5</td>
+          <td className="columns">Data 6</td>
+        </tr>
+        <tr className="columns"lassName="rows">
+          <td className="columns">Data 1</td>
+          <td className="columns">Data 2</td>
+          <td className="columns">Data 3</td>
+          <td className="columns">Data 4</td>
+          <td className="columns">Data 5</td>
+          <td className="columns">Data 6</td>
+        </tr>
+        <tr className="columns"lassName="rows">
+          <td className="columns">Data 1</td>
+          <td className="columns">Data 2</td>
+          <td className="columns">Data 3</td>
+          <td className="columns">Data 4</td>
+          <td className="columns">Data 5</td>
+          <td className="columns">Data 6</td>
+        </tr>
+        <tr className="columns"lassName="rows">
+          <td className="columns">Data 1</td>
+          <td className="columns">Data 2</td>
+          <td className="columns">Data 3</td>
+          <td className="columns">Data 4</td>
+          <td className="columns">Data 5</td>
+          <td className="columns">Data 6</td>
+        </tr>
+        <tr className="columns"lassName="rows">
+          <td className="columns">Data 1</td>
+          <td className="columns">Data 2</td>
+          <td className="columns">Data 3</td>
+          <td className="columns">Data 4</td>
+          <td className="columns">Data 5</td>
+          <td className="columns">Data 6</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default MoreResults;
