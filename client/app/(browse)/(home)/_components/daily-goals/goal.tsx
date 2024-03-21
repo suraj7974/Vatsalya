9f7be7c9b52a5a6309f74645edf2c9d83bdd39e0
suraj7974 <surajpatelsuraj55@gmail.com>
2024-03-22 03:36:36 +0530

    fixed colors

--- client/app/(browse)/(home)/_components/daily-goals/goal.tsx
+++ client/app/(browse)/(home)/_components/daily-goals/goal.tsx
@@ -13,7 +13,7 @@ export const Goal = ({
 }: GoalProps) => {
     return (
         <div className="h-[400px] rounded-lg p-3 m-4 flex flex-col gap-y-4 border-slate-400 shadow-[0_2px_12px_4px] shadow-slate-300 mx-auto">
-            <div className="h-2/3 w-full bg-slate-800 flex flex-col text-white gap-y-5 items-center justify-center rounded-md">
+            <div className="h-2/3 w-full bg-black flex flex-col text-white gap-y-5 items-center justify-center rounded-md">
                 <Icon className="w-16 h-16" />
                 <h3 className="text-2xl">{name}</h3>
             </div>
